922a009d6c3f57944b9ea4f31e40f4852a561728
Leandro Adrian da Silva <leandrinsilva22@gmail.com>
2024-11-15 14:54:17 -0300

    add not found

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,9 @@
+<template>
+    <q-card flat class="colum text-center q-gutter-sm q-mt-xl">
+        <q-card-section>
+            <q-icon name="warning" size="48px" />
+            <h2 class="q-mt-md">Oops! Página não encontrada</h2>
+            <q-btn to="/" icon="home" flat color="primary" label="Ir para o início " />
+        </q-card-section>
+    </q-card>
+</template>
